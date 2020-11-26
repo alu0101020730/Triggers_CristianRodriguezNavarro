ca37e291cff341d50c84cd6af279aa4881153271
Cristian Rodriguez <43813175+alu0101020730@users.noreply.github.com>
2020-11-26 12:58:12 +0000

    arreglo

--- ej2.sql
+++ ej2.sql
@@ -11,7 +11,9 @@ END //
 
 CREATE TRIGGER trigger_update_persona_vive_en_una_unica_vivienda BEFORE UPDATE ON PERSONA FOR EACH ROW
 BEGIN
-    IF NEW.PISO_CALLE IS NOT NULL and NEW.VIVIENDA_CALLE IS NOT NULL  THEN
+    IF OLD.PISO_CALLE IS NOT NULL and NEW.VIVIENDA_CALLE IS NOT NULL  THEN
+        signal sqlstate '45000' set message_text = 'No puede tener dos domicilios';
+    ELSEIF NEW.PISO_CALLE IS NOT NULL and OLD.VIVIENDA_CALLE IS NOT NULL  THEN
         signal sqlstate '45000' set message_text = 'No puede tener dos domicilios';
     END IF;
 
